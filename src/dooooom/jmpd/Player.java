e0e49c3d5e72e0565ddd6ee2400bfcba7eb8edd5
Alittlemurkling <zanethefallen@gmail.com>
2014-04-16 01:45:10 -0400

    Added get methods for the current track

--- src/dooooom/jmpd/Player.java
+++ src/dooooom/jmpd/Player.java
@@ -7,7 +7,9 @@ import javafx.scene.media.MediaException;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import org.omg.CORBA.UNKNOWN;
 
+import javax.print.attribute.standard.MediaPrintableArea;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
@@ -34,17 +36,10 @@ public class Player extends Application {
         PlayerControl control = new PlayerControl();
         Thread controllerThread = new Thread(control);
         controllerThread.start();
-//        addSongs();
         setPlayQueue();
-//        playQueue.get(0).play();
     }
 
     public void setPlayQueue() {
-//        for (String path : playQueueFiles) {
-//            System.out.println(path);
-//            MediaPlayer p = new MediaPlayer(new Media(path));
-//            playQueue.add(p);
-//        }
         for (int i = 0; i < playQueue.size(); i++) {
             if (i < playQueue.size() - 1) {
                 final int t = i + 1;
@@ -124,36 +119,6 @@ public class Player extends Application {
             playQueue.add(p);
         }
         playQueueFiles.addAll(newSongs);
-    }
-
-    //TODO: remove main and launch from Server as a thread
-    public static void main(String[] args) {
-        launch(args);
-    }
-
-    public class PlayerControl implements Runnable {
-        public void run() {
-            Scanner in = new Scanner(System.in);
-            try {
-                while (true) {
-                    String input = in.nextLine();
-                    System.out.println(input);
-                    if (input.equals("t")) {
-                        toggle();
-                    } else if (input.equals("n")) {
-                        next();
-                    } else if (input.equals("p")) {
-                        prev();
-                    } else if (input.equals("s")) {
-                        stop();
-                    } else if (input.equals("a")) {
-                        addSongs();
-                    }
-                }
-            } catch (NoSuchElementException e) {
-                in.close();
-            }
-        }
     }
 
     public void toggle() {
@@ -236,4 +201,51 @@ public class Player extends Application {
             nextTrackIndex = current - 1;
         return nextTrackIndex;
     }
+
+    public MediaPlayer getCurrent() {
+        if(playQueue.isEmpty())
+            return null;
+        else
+            return playQueue.get(currentPlayer);
+    }
+
+    public double getTime() {
+        if(!playQueue.isEmpty())
+            return getCurrent().getCurrentTime().toSeconds();
+        else
+            return 0.0;
+    }
+
+    //TODO: remove main and launch from Server as a thread
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public class PlayerControl implements Runnable {
+        public void run() {
+            Scanner in = new Scanner(System.in);
+            try {
+                while (true) {
+                    String input = in.nextLine();
+                    System.out.println(input);
+                    if (input.equals("t")) {
+                        toggle();
+                    } else if (input.equals("n")) {
+                        next();
+                    } else if (input.equals("p")) {
+                        prev();
+                    } else if (input.equals("s")) {
+                        stop();
+                    } else if (input.equals("a")) {
+                        addSongs();
+                    } else if (input.equals("c")) {
+                        System.out.println(getTime());
+                    }
+                }
+            } catch (NoSuchElementException e) {
+                in.close();
+            }
+        }
+    }
+
 }
