48f28ce4cd6c4189bf88f83eccd7cb6d2fff6641
Alittlemurkling <zanethefallen@gmail.com>
2014-05-17 17:09:14 -0400

    Removed songs

--- src/dooooom/jmpd/Player.java
+++ src/dooooom/jmpd/Player.java
@@ -1,45 +1,43 @@
 package dooooom.jmpd;
 
 import javafx.application.Application;
-import javafx.application.Platform;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaException;
 import javafx.scene.media.MediaPlayer;
 import javafx.stage.Stage;
-import javafx.util.Duration;
-import org.omg.CORBA.UNKNOWN;
 
-import javax.print.attribute.standard.MediaPrintableArea;
 import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.lang.reflect.Array;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.util.*;
 
 public class Player extends Application {
-    public enum Command {
-        NULL, TOGGLE, PAUSE, PLAY, STOP, PREV,
-        NEXT, ADD, ADDTOPLAYLIST, REM,
-        REMPLAYLIST, DEL, ACK
-    }
 
-    private ArrayList<String> playQueueFiles = new ArrayList<String>();
-    private ArrayList<MediaPlayer> playQueue = new ArrayList<MediaPlayer>();
-    private boolean loopingRepeat = false;
-    private int currentPlayer;
+    private static ArrayList<String> playQueueFiles = new ArrayList<String>();
+    private static ArrayList<MediaPlayer> playQueue = new ArrayList<MediaPlayer>();
+    private static boolean loopingRepeat = false;
+    private static int currentPlayer;
+
+//    public Player() {
+//        PlayerControl p = new PlayerControl();
+//    }
 
     @Override
     public void start(Stage arg0) {
-        PlayerControl control = new PlayerControl();
-        Thread controllerThread = new Thread(control);
-        controllerThread.start();
-        setPlayQueue();
+        try {
+            UDPServer server  = new UDPServer();
+            Thread serverThread = new Thread(server);
+            serverThread.start();
+
+            PlayerControl control = new PlayerControl();
+            Thread controllerThread = new Thread(control);
+            controllerThread.start();
+
+            setPlayQueue();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
-    public void setPlayQueue() {
+    public static void setPlayQueue() {
         for (int i = 0; i < playQueue.size(); i++) {
             if (i < playQueue.size() - 1) {
                 final int t = i + 1;
@@ -47,7 +45,7 @@ public class Player extends Application {
                     @Override
                     public void run() {
                         currentPlayer = t;
-                        playQueue.get(t).play();
+                        play();
                     }
                 });
             } else if (loopingRepeat) {
@@ -57,11 +55,118 @@ public class Player extends Application {
                         playQueue.clear();
                         setPlayQueue();
                         currentPlayer = 0;
-                        playQueue.get(0).play();
+                        play();
                     }
                 });
             }
         }
+    }
+
+    public static void add(ArrayList<String> newSongs) {
+        for (String s : newSongs) {
+            String path = s.replace(" ", "%20");
+            System.out.println(path);
+            final MediaPlayer p = new MediaPlayer(new Media(path));
+            playQueue.add(p);
+        }
+        playQueueFiles.addAll(newSongs);
+    }
+
+    public static void toggle() {
+        try {
+            if (!playQueue.isEmpty()) {
+                if (playQueue.get(currentPlayer).getStatus() == MediaPlayer.Status.PLAYING) {
+                    pause();
+                } else {
+                    play();
+                }
+            }
+        } catch (MediaException e) {
+            System.out.println("Invalid media type.");
+        }
+    }
+
+    public static void pause() {
+        try {
+            if (!playQueue.isEmpty())
+                playQueue.get(currentPlayer).pause();
+        } catch (MediaException e) {
+            System.out.println("Invalid media type.");
+        }
+    }
+
+    public static void play() {
+        try {
+            if (!playQueue.isEmpty()) {
+                playQueue.get(currentPlayer).play();
+                System.out.println("Now playing: " + playQueueFiles.get(currentPlayer));
+            }
+        } catch (MediaException e) {
+            System.out.println("Invalid media type.");
+        }
+    }
+
+    public static void stopPlayback() {
+        try {
+            if (!playQueue.isEmpty())
+                playQueue.get(currentPlayer).stop();
+        } catch (MediaException e) {
+            System.out.println("Invalid media type.");
+        }
+    }
+
+    public static void next() {
+        try {
+            if (!playQueue.isEmpty()) {
+                playQueue.get(currentPlayer).stop();
+                currentPlayer = getNextTrackIndex(currentPlayer);
+                if (!((currentPlayer == 0)   //Exclude the corner case where we're at the
+                        && !loopingRepeat)) //   end of the play queue and not repeating
+                    play();
+            }
+        } catch (MediaException e) {
+            System.out.println("Invalid media type.");
+        }
+    }
+
+    public static int getNextTrackIndex(int current) {
+        int nextTrackIndex = 0;
+        if (current != playQueue.size() - 1)
+            nextTrackIndex = current + 1;
+        return nextTrackIndex;
+    }
+
+    public static void prev() {
+        try {
+            if (!playQueue.isEmpty()) {
+                stopPlayback();
+                currentPlayer = getPrevTrackIndex(currentPlayer);
+                play();
+            }
+        } catch (MediaException e) {
+            System.out.println("Invalid media type.");
+        }
+    }
+
+    public static int getPrevTrackIndex(int current) {
+        int nextTrackIndex = playQueue.size() - 1;
+        if (current != 0)
+            nextTrackIndex = current - 1;
+        return nextTrackIndex;
+    }
+
+    public static MediaPlayer getCurrent() {
+        if(playQueue.isEmpty())
+            return null;
+        else
+            return playQueue.get(currentPlayer);
+    }
+
+    public static double getTime() {
+        if(!playQueue.isEmpty())
+            return getCurrent().getCurrentTime().toSeconds();
+        else
+            return 0.0;
     }
 
     //Testing only
@@ -86,7 +191,7 @@ public class Player extends Application {
      */
     {
         String s = System.getProperty("file.separator");
-        File musicFolder = new File("/home/zap/music/Sleigh Bells/Reign of Terror/");
+        File musicFolder = new File("/home/zap/music/Rustie/Glass Swords/");
 
         File[] abstractPaths = musicFolder.listFiles();
 
@@ -111,119 +216,16 @@ public class Player extends Application {
         return arrayToReturn;
     } //end Testing
 
-    public void add(ArrayList<String> newSongs) {
-        for (String s : newSongs) {
-            String path = s.replace(" ", "%20");
-            System.out.println(path);
-            final MediaPlayer p = new MediaPlayer(new Media(path));
-            playQueue.add(p);
-        }
-        playQueueFiles.addAll(newSongs);
-    }
-
-    public void toggle() {
-        try {
-            if (!playQueue.isEmpty()) {
-                if (playQueue.get(currentPlayer).getStatus() == MediaPlayer.Status.PLAYING) {
-                    playQueue.get(currentPlayer).pause();
-                } else {
-                    playQueue.get(currentPlayer).play();
-                }
-            }
-        } catch (MediaException e) {
-            System.out.println("Invalid media type.");
-        }
-    }
-
-    public void pause() {
-        try {
-            if (!playQueue.isEmpty())
-                playQueue.get(currentPlayer).pause();
-        } catch (MediaException e) {
-            System.out.println("Invalid media type.");
-        }
-    }
-
-    public void play() {
-        try {
-            if (!playQueue.isEmpty())
-                playQueue.get(currentPlayer).play();
-        } catch (MediaException e) {
-            System.out.println("Invalid media type.");
-        }
-    }
-
-    public void stop() {
-        try {
-            if (!playQueue.isEmpty())
-                playQueue.get(currentPlayer).stop();
-        } catch (MediaException e) {
-            System.out.println("Invalid media type.");
-        }
-    }
-
-    public void next() {
-        try {
-            if (!playQueue.isEmpty()) {
-                playQueue.get(currentPlayer).stop();
-                currentPlayer = getNextTrackIndex(currentPlayer);
-                if (!((currentPlayer == 0)   //Exclude the corner case where we're at the
-                        && !loopingRepeat)) //   end of the play queue and not repeating
-                    playQueue.get(currentPlayer).play();
-            }
-        } catch (MediaException e) {
-            System.out.println("Invalid media type.");
-        }
-    }
-
-    public int getNextTrackIndex(int current) {
-        int nextTrackIndex = 0;
-        if (current != playQueue.size() - 1)
-            nextTrackIndex = current + 1;
-        return nextTrackIndex;
-    }
-
-    public void prev() {
-        try {
-            if (!playQueue.isEmpty()) {
-                playQueue.get(currentPlayer).stop();
-                currentPlayer = getPrevTrackIndex(currentPlayer);
-                playQueue.get(currentPlayer).play();
-            }
-        } catch (MediaException e) {
-            System.out.println("Invalid media type.");
-        }
-    }
-
-    public int getPrevTrackIndex(int current) {
-        int nextTrackIndex = playQueue.size() - 1;
-        if (current != 0)
-            nextTrackIndex = current - 1;
-        return nextTrackIndex;
-    }
-
-    public MediaPlayer getCurrent() {
-        if(playQueue.isEmpty())
-            return null;
-        else
-            return playQueue.get(currentPlayer);
-    }
-
-    public double getTime() {
-        if(!playQueue.isEmpty())
-            return getCurrent().getCurrentTime().toSeconds();
-        else
-            return 0.0;
-    }
-
     //TODO: remove main and launch from Server as a thread
     public static void main(String[] args) {
         launch(args);
     }
 
     public class PlayerControl implements Runnable {
+//        public PlayerControl() {}
         public void run() {
             Scanner in = new Scanner(System.in);
+            UDPClient client = new UDPClient();
             try {
                 while (true) {
                     String input = in.nextLine();
@@ -235,11 +237,17 @@ public class Player extends Application {
                     } else if (input.equals("p")) {
                         prev();
                     } else if (input.equals("s")) {
-                        stop();
+                        stopPlayback();
                     } else if (input.equals("a")) {
                         addSongs();
                     } else if (input.equals("c")) {
                         System.out.println(getTime());
+                    } else if (input.equals("m")) {
+                        try {
+                            client.sendMessage(UDPServer.Command.TOGGLE, "");
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
                     }
                 }
             } catch (NoSuchElementException e) {
